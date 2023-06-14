e303d4633582e9c3cf4bdffaf22d298378826169
loper <bizrenay@gmail.com>
2023-06-14 21:51:20 +0300

    initial commit

---
+++ src/main/java/me/loper/logger/Logger.java
@@ -0,0 +1,15 @@
+package me.loper.logger;
+
+public interface Logger {
+
+    void info(String s);
+
+    void warn(String s);
+
+    void warn(String s, Throwable t);
+
+    void severe(String s);
+
+    void severe(String s, Throwable t);
+
+}
